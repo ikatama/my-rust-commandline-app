100e6e5c21c56fa15710a13f5ca3248cb577aa98
ikatama <ikatama@example.com>
2023-11-13 15:37:30 +0000

    parsing command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// 指定したファイルからパターンに合致した行を探す。
+/// 合致した行をコンソールに表示する。
+#[derive(Parser)]
+struct Cli {
+    /// 探すパターン
+    #[arg(short = 'p', long = "pattern", default_value_t = String::from("test"))]
+    pattern: String,
+    /// パターンを探す対象のファイルパス
+    #[arg(long = "path")]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
